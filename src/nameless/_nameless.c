3f0e01e0114e5c47920650bb3c00701a6eb959a3
Ionel Cristian Mărieș <contact@ionelmc.ro>
2017-10-25 23:31:21 +0300

    Up.

--- src/nameless/_nameless.c
+++ src/nameless/_nameless.c
@@ -1,10 +1,10 @@
 
 char* longest(int argc, char *argv[]) {
     if (argc) {
-        int len,
+        int len, i,
             max = 0,
             pos = 0;
-        for (int i = 0; i < argc; i++) {
+        for (i = 0; i < argc; i++) {
             len = strlen(argv[i]);
             if (len > max) {
                 max = len;
