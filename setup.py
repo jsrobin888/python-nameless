7d3f9089b7854eefc95e9e13fd43d52a7163bfd9
Ionel Cristian Mărieș <contact@ionelmc.ro>
2018-10-14 15:37:16 +0300

    Up.

--- setup.py
+++ setup.py
@@ -18,10 +18,11 @@ from setuptools import setup
 
 
 def read(*names, **kwargs):
-    return io.open(
+    with io.open(
         join(dirname(__file__), *names),
         encoding=kwargs.get('encoding', 'utf8')
-    ).read()
+    ) as fh:
+        return fh.read()
 
 
 # Enable code coverage for C code: we can't use CFLAGS=-coverage in tox.ini, since that may mess with compiling
